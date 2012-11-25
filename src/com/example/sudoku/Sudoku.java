127bc7bf53bba34b0216e2b8147dcc12fdeea7ab
lesnikovskiy <lesnikovski@gmail.com>
2012-11-25 22:18:50 +0200

    Functional Sudoku game

--- src/com/example/sudoku/Sudoku.java
+++ src/com/example/sudoku/Sudoku.java
@@ -8,8 +8,13 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.util.Log;
 
 public class Sudoku extends Activity implements OnClickListener {
+	private static final String TAG = "Sudoku";
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +48,7 @@ public class Sudoku extends Activity implements OnClickListener {
     	switch(item.getItemId()) {
 	    	case R.id.settings:
 	    		startActivity(new Intent(this, Settings.class));
-	    		return true;
+	    		return true;	    	
     	}
     	
     	return false;
@@ -55,6 +60,32 @@ public class Sudoku extends Activity implements OnClickListener {
 	    		Intent i = new Intent(this, About.class);
 	    		startActivity(i);
 	    		break;
+	    		
+	    	case R.id.new_game_button:
+	    		openNewGameDialog();
+	    		break;
+	    		
+	    	case R.id.exit_button:
+	    		finish();
+	    		break;
     	}
+    }
+    
+    private void openNewGameDialog() {
+    	new AlertDialog.Builder(this)
+    	.setTitle(R.string.new_game_title)
+    	.setItems(R.array.difficulty,
+    			new DialogInterface.OnClickListener() {					
+					public void onClick(DialogInterface dialogInterface, int which) {
+						startGame(which);						
+					}
+				}).show();
+    }
+    
+    private void startGame(int whichGame) {
+    	Log.d(TAG, "Clicked on " + whichGame);
+    	Intent intent = new Intent(Sudoku.this, Game.class);
+    	intent.putExtra(Game.KEY_DIFFICULTY, whichGame);
+    	startActivity(intent);
     }
 }
